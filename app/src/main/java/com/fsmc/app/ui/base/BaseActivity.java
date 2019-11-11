126f8554c06ef8ff83c07cd2d8622f60118d4e14
viktor <viktor.mushak@gmail.com>
2019-11-11 19:52:48 +0200

    some message

--- app/src/main/java/com/fsmc/app/ui/base/BaseActivity.java
+++ app/src/main/java/com/fsmc/app/ui/base/BaseActivity.java
@@ -4,7 +4,7 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class BaseActivity extends AppCompatActivity {
+public abstract class BaseActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
